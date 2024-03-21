f430c03b08a90ae419be7f20ff399645c97f69a7
mohamed <mohameddrame907@gmail.com>
2024-03-21 18:14:24 +0100

    add object recognition project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,6 +10,7 @@ import Portfolio from "../../Assets/Projects/Portfolio.png";
 import SpaceInvaders from "../../Assets/Projects/SpaceInvaders.png";
 import theatromatic from "../../Assets/Projects/theatromatic.png";
 import weathr from "../../Assets/Projects/weather.png";
+import OR from "../../Assets/Projects/ObjectRecognition.png"
 
 function Projects() {
   return (
@@ -25,6 +26,7 @@ function Projects() {
             <ProjectCard
               imgPath={pokemon}
               isBlog={false}
+              isGithub={true}
               title="Pokemon"
               description="Vous êtes un chasseur de Pokémon ayant la possibilité de capturer des Pokémon à travers un quiz (incluant cris, taille, poids, type). Vous pouvez également retrouver vos Pokémon capturés dans la section Pokédex."
               ghLink="https://github.com/MohamedNiaissa/PokeApp"
@@ -35,6 +37,7 @@ function Projects() {
             <ProjectCard
               imgPath={weathr}
               isBlog={false}
+              isGithub={true}
               title="Weather"
               description="Cette application vous permet de regarder la météo ainsi que les mesures astronomiques actuel provenant de plusieurs ville dans le monde. Vous avez également à votre disposition la liste des villes consultées. "
               ghLink="https://github.com/MohamedNiaissa/weathr"
@@ -45,16 +48,29 @@ function Projects() {
             <ProjectCard
               imgPath={theatromatic}
               isBlog={false}
+              isGithub={true}
               title="Theatromatic"
               description="Theatromatic est une application de génération de pièce de théâtre. Votre pièce de théâtre se base sur votre scénario et les personnages que vous avez crée."
               ghLink="https://github.com/LeoJeanjean/Theatromatic"
             />
           </Col>
-
+          </Row>
+          <Row>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={OR}
+              isBlog={false}
+              isGithub={false}
+              title="Reconnaisseur d'objet"
+              description="Le but de cette application est de reconnaitre les objets pris en photo. Le but de ce projet est de surtout apprendre à améliorer l'architecture de notre code et de nos fichiers. "
+            />
+          </Col>
+          
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={SpaceInvaders}
               isBlog={false}
+              isGithub={true}
               title="SpaceInvaders et Memory Cards"
               description="Cette application regroupe les deux jeux populaires: Space Invaders et le Memory Game. Le temps pris pour chaque partie de Memory est visible et classé."
               ghLink="https://github.com/MohamedNiaissa/ProjetJsAvancee"
@@ -65,27 +81,31 @@ function Projects() {
             <ProjectCard
               imgPath={CDL}
               isBlog={false}
+              isGithub={true}
               title="Chiffres et des lettres"
               description="Il s'agit du jeu classique des chiffres et des lettres. Vous pouvez jouez à deux ou contre la machine."
               ghLink="https://github.com/MohamedNiaissa/chiffrelettres-dcedl-david-mohamed-angel-kevin"
             />
           </Col>
+          </Row>
+          <Row>
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={JavaFx}
               isBlog={false}
+              isGithub={true}
               title="Multi-Applications"
               description="Plusieurs applications sont regroupé dans ce projet: gestionnaire de librairie; gestionnaire d'une armée; calculateur IMC; convertisseur de decimal vers hexadecimal, binaire, romain"
               ghLink="https://github.com/MohamedNiaissa/ProjetJavaAv"
             />
           </Col>
-        </Row>
-        <Row>
+        
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={Battle}
               isBlog={false}
+              isGithub={true}
               title="Battle simulation"
               description="Il s'agit d'un simulateur de combat tour par tour. PLusieurs types de combattant sont disponible tel que paysan, archer, fou, magicien..."
               ghLink="https://github.com/MohamedNiaissa/Projet_java"
@@ -95,11 +115,11 @@ function Projects() {
             <ProjectCard
               imgPath={Portfolio}
               isBlog={false}
+              isGithub={true}
               title="Clone Portfolio"
               description="C'est un clone d'un portfolio basé sur une maquette qui nous a été fournis."
               ghLink="https://github.com/MohamedNiaissa/portfolioclone"
               demoLink="https://mohamedniaissa.github.io/portfolioclone/"
-
             />
           </Col>
         </Row>
